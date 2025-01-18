92cc1ce00b4a174addfed7205c31317517829260
Leonardo Hirsch <leonardo.hirsch@koochapps.com>
2025-01-18 17:30:43 +0100

    remove validation un admin functions of setup

--- includes/Setup.php
+++ includes/Setup.php
@@ -17,40 +17,25 @@ class Setup {
         add_action('plugins_loaded', [self::class, 'load_textdomain']);
         add_action('init', [self::class, 'register_custom_post_type']);
         add_action('init', [self::class, 'register_taxonomy']);
-        add_action('admin_menu', [self::class, 'add_admin_menu']);
+        add_action('admin_menu', [self::class, 'add_admin_pages']);
     }
 
-    public static function add_admin_menu()
+    public static function add_admin_pages()
     {
-        if (current_user_can('manage_options')) {
-            new VassRickMorty\Admin\CharacterImportPage(
-                new VassRickMorty\Includes\CharacterImporter()
-            );            
-        }        
+        self::admin_import_page(); 
+        self::admin_settings_page();   
+    }
+
+    public static function admin_import_page()
+    {
+        new VassRickMorty\Admin\CharacterImportPage(
+            new VassRickMorty\Includes\CharacterImporter()
+        );            
     }
 
     public static function admin_settings_page()
     {
-        if (!current_user_can('manage_options')) {
-            wp_die(__('You do not have permissions to access this page.', RICK_MORTY_TEXT_DOMAIN));
-        }
 
-        if (isset($_GET['success'])) {
-            if ($_GET['success'] === '1') {
-                echo '<div class="notice notice-success is-dismissible"><p>' . __('Characters imported successfully.', RICK_MORTY_TEXT_DOMAIN) . '</p></div>';
-            } elseif ($_GET['success'] === '0') {
-                echo '<div class="notice notice-error is-dismissible"><p>' . __('Failed to import characters.', RICK_MORTY_TEXT_DOMAIN) . '</p></div>';
-            }
-        }
-
-        echo '<div class="wrap">';
-        echo '<h1>' . esc_html(get_admin_page_title()) . '</h1>';
-        echo '<form method="post" action="' . esc_html(admin_url('admin-post.php')) . '">';
-        wp_nonce_field('import_rick_morty_action', 'import_rick_morty_nonce');
-        echo '<input type="hidden" name="action" value="import_rick_morty_characters">';
-        echo '<input type="submit" class="button-primary" value="' . esc_attr__('Import Characters', RICK_MORTY_TEXT_DOMAIN) . '">';
-        echo '</form>';
-        echo '</div>';
     }
 
 
