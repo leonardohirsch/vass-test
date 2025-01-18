3d4760c184c83994eb69128012dba1dc6457f827
Leonardo Hirsch <leonardo.hirsch@koochapps.com>
2025-01-18 16:52:14 +0100

    feat: import.js

---
+++ admin/js/import.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function ($) {
+  $("#import-characters").on("click", function () {
+    var import_button = $(this);
+    import_button.prop("disabled", true);
+    $("#import-loading-message").show();
+
+    $.ajax({
+      url: rickMortyAjax.ajaxurl,
+      type: "POST",
+      dataType: "json",
+      data: {
+        action: "import_characters",
+        _ajax_nonce: rickMortyAjax.nonce,
+      },
+      success: function (response) {
+        if (response.success) {
+          $("#import-messages").html(
+            '<div class="notice notice-success is-dismissible"><p>' +
+              response.data.message +
+              "</p></div>"
+          );
+        } else {
+          $("#import-messages").html(
+            '<div class="notice notice-error is-dismissible"><p>' +
+              response.data.message +
+              "</p></div>"
+          );
+        }
+      },
+      error: function (xhr, status, error) {
+        var errorMessage = xhr.status + ": " + xhr.statusText;
+        $("#import-messages").html(
+          '<div class="notice notice-error is-dismissible"><p>' +
+            errorMessage +
+            "</p></div>"
+        );
+      },
+      complete: function () {
+        $("#import-loading-message").hide();
+        import_button.prop("disabled", false);
+      },
+    });
+  });
+});
